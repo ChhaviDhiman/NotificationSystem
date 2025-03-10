e0ad9913c59ddd998ed07e64ef2a662f7ab5b89a
ChhaviDhiman <dhimanchhavi5@gmail.com>
2025-03-10 18:32:10 +0530

    added

--- NotificationSystem/settings.py
+++ NotificationSystem/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,6 +56,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "NotificationSystem.urls"
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
@@ -70,6 +72,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+CORS_ALLOWED_ORIGINS = [
+    "https://aicaradvisor.com",
+    "https://your-other-allowed-domain.com"
 ]
 
 WSGI_APPLICATION = "NotificationSystem.wsgi.application"
